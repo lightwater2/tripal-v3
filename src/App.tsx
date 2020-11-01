2021f9a7cd3428d5f0981c41c7d0183dca6d5ab5
yoknono <yoknono@kakao.com>
2020-11-01 23:05:02 +0900

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 
+import Home from 'pages/Home';
 
+import Footer from 'components/Footer';
 export default () => (
   <>
-
+    <Home/>
+    <Footer/>
   </>
 )
---
+++ src/styled/components.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FullSizeBanner =  styled.div< { img:string, width:number, height:number }>`
+  background-image: url(${(props) => props.img});
+  width: 100%;
+  height: 0;
+  padding-bottom: calc(${(props) => props.height/props.width} * 100%);
+  background-size:cover;
+  background-repeat: no-repeat;
+`
